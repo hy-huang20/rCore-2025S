0584b673d5838f0240f3767d5c784a36a55d90c1
hy-huang20 <1479054529@qq.com>
2025-05-08 02:46:57 +0800

    sys_get_time 完成

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,9 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{
+    task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, current_user_token},
+    timer::get_time_us,
+    mm::translated_byte_buffer,
+};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -27,7 +31,25 @@ pub fn sys_yield() -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let us = get_time_us();
+    let ts = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let size_of_timeval = core::mem::size_of::<TimeVal>();
+    let buffers = translated_byte_buffer(current_user_token(), _ts as *const u8, size_of_timeval);
+    let ts_byte_arr: &[u8] = unsafe {
+        core::slice::from_raw_parts(
+            &ts as *const TimeVal as *const u8,
+            size_of_timeval
+        )
+    };
+    let mut ts_idx: usize = 0;
+    for buffer in buffers {
+        buffer.copy_from_slice(&ts_byte_arr[ts_idx..ts_idx+buffer.len()]);
+        ts_idx += buffer.len();
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
