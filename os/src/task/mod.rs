787f65b9143ff74dfbf487f98608cc434e467cdd
hy-huang20 <1479054529@qq.com>
2025-05-12 04:36:38 +0800

    ch5 之前实验接口适配完成，完成 spawn

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -21,6 +21,7 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
+use crate::mm::{VirtPageNum, MapPermission, VirtAddr, StepByOne};
 use crate::loader::get_app_data_by_name;
 use alloc::sync::Arc;
 use lazy_static::*;
@@ -114,4 +115,48 @@ lazy_static! {
 ///Add init process to the manager
 pub fn add_initproc() {
     add_task(INITPROC.clone());
+}
+
+///
+pub fn map_for_current_task(start_vpn: VirtPageNum, num_pages: usize, map_perm: MapPermission) -> isize {
+    let cur_task = current_task().unwrap();
+    let memory_set = &mut cur_task.inner_exclusive_access().memory_set;
+    let mut end_vpn = start_vpn;
+    for _ in 0..num_pages {
+        if let Some(pte) = memory_set.translate(end_vpn) {
+            if pte.is_valid() { // vpn 已经被映射到了已经存在的物理页
+                return -1;
+            }
+        }
+        end_vpn.step();
+    }
+    let start_va = VirtAddr::from(start_vpn);
+    let end_va = VirtAddr::from(end_vpn);
+    // 一块新的 MapArea，会自动 map PageTable
+    memory_set.insert_framed_area(start_va, end_va, map_perm);
+    return 0;
+}
+
+///
+pub fn unmap_for_current_task(start_vpn: VirtPageNum, num_pages: usize) -> isize {
+    let cur_task = current_task().unwrap();
+    let memory_set = &mut cur_task.inner_exclusive_access().memory_set;
+    let mut end_vpn = start_vpn;
+    for _ in 0..num_pages {
+        if let Some(pte) = memory_set.translate(end_vpn) {
+            if !pte.is_valid() { // 不能解除不存在的映射
+                return -1;
+            }
+            // PageTable 里面的 frames 存储了页表所有节点对应的物理帧，这些帧用来存 pte
+            // 而 MapArea 里面的 data_frames 存储了页表三级节点中的最后一级（叶节点）指向的物理帧，这些帧用来存数据
+            // 这里并没有释放掉 MapArea，但从 PageTable 中去掉了对 MapArea 中相应 data_frames 的映射
+            //（当然，可能并非该 MapArea 全部 data_frames），因此该 MapArea 中再无法通过 PageTable 映射到的 data_frames
+            // 对后续也没有影响了，没有被取消映射因此仍可以通过 PageTable 映射到的哪些 data_frames 没有受到影响  
+            memory_set.unmap_from_page_table(end_vpn);
+            end_vpn.step();
+        } else { // 不能解除不存在的映射
+            return -1;
+        }
+    }
+    return 0;
 }
